7ab98c47922823ae1a372603511951cddda3ee2c
Ahmet Çetinkaya <ahmet4cetinkaya@outlook.com>
2021-05-11 19:34:22 +0300

    🎉 Initial Commit

---
+++ src/main/java/ahmetcetinkaya/HRMSProjectBackend/HrmsProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package ahmetcetinkaya.HRMSProjectBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmsProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmsProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ahmetcetinkaya/HRMSProjectBackend/HrmsProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ahmetcetinkaya.HRMSProjectBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmsProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
