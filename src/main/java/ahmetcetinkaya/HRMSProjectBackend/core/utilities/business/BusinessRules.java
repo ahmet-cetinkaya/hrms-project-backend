9bc851395381b8f86ddeb611e5e2edb11ff21a97
Ahmet Çetinkaya <ahmet4cetinkaya@outlook.com>
2021-05-27 11:19:20 +0300

    ✨(Core) Business Rules

---
+++ src/main/java/ahmetcetinkaya/HRMSProjectBackend/core/utilities/business/BusinessRules.java
@@ -0,0 +1,13 @@
+package ahmetcetinkaya.HRMSProjectBackend.core.utilities.business;
+
+import ahmetcetinkaya.HRMSProjectBackend.core.utilities.results.Result;
+import ahmetcetinkaya.HRMSProjectBackend.core.utilities.results.SuccessResult;
+
+public class BusinessRules {
+	public static Result run(final Result... logics) {
+		for (final Result logic : logics)
+			if (!logic.isSuccess())
+				return logic;
+		return new SuccessResult();
+	}
+}
