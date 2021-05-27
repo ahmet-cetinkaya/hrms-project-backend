362b7c79970cdb549a74654e044a574e73058a25
Ahmet Çetinkaya <ahmet4cetinkaya@outlook.com>
2021-05-27 11:18:24 +0300

    ✅ (Core) Email Service

---
+++ src/main/java/ahmetcetinkaya/HRMSProjectBackend/core/utilities/helpers/email/EmailService.java
@@ -0,0 +1,7 @@
+package ahmetcetinkaya.HRMSProjectBackend.core.utilities.helpers.email;
+
+import ahmetcetinkaya.HRMSProjectBackend.core.utilities.results.Result;
+
+public interface EmailService {
+	Result send(String to, String title, String body);
+}
